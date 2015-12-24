ba2ae0b96401d7a36fdecd72727d707d9c5c5ecd
natac13 <sean.campbell13@gmail.com>
2015-12-24 13:39:48 -0500

    installed cross-env

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
-import React      from 'react';
-import { render } from 'react-dom';
-import Router     from 'react-router';
-import routes     from './config/routes';
+import React             from 'react';
+import { render }        from 'react-dom';
+import Router            from 'react-router';
+import { createHistory } from 'history';
+import routes            from './config/routes';
+
+import { syncReduxAndRouter } from 'redux-simple-router'
+
+
 import Main from './components/Main.js';
 
 
@@ -11,10 +16,11 @@ console.log(store.getState())
 import './scss/main.scss';
 
 
-const rootElement = document.getElementById('root');
 
+const rootElement = document.getElementById('root');
+const histroy = createHistory();
 render((
-  <Router>
+  <Router history={history} >
     {routes}
   </Router>
 ), rootElement);
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
+import { routeReducer } from 'redux-simple-router'
 
 import recipeList from './recipeList';
 
-const rootReducer = combineReducers({
+const rootReducer = combineReducers(Object.assign({}, {
     recipeList
-});
+}, {
+    routing: routeReducer
+}));
 
 export default rootReducer;
