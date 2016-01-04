36912a2e3569f3ee99da7ce6366c29041118f403
natac13 <sean.campbell13@gmail.com>
2016-01-03 19:28:18 -0500

    chore(Immutable DevTools!): I added Immutable dev tools which is jsut amazing!! https://github.com/a

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import { routeReducer }    from 'redux-simple-router';
 
+import Immutable from 'immutable';
+import installDevTools from 'immutable-devtools';
+installDevTools(Immutable);
+
 import recipeList   from './recipeList';
 import asyncRequest from './asyncRequest';
 
