0c04ef6f5f274fc623876f6d676ab4bd7040c61f
natac13 <sean.campbell13@gmail.com>
2016-01-16 11:01:45 -0500

    feat(RecipeDetails):
    Have got a basic layout done for the recipeDetails. I do not know how I am going to get the files
    uploaded yet through this app. However I can upload to Cloudinary and store that url in the recipe
    object, under  key. Will need to add in the edit recipe and add recipe a field for this
    data. Will open an issue for that.

--- app/components/recipeDetails/RecipeDetails.js
+++ app/components/recipeDetails/RecipeDetails.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 
-import style from './style';
+import style from './style.scss';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -9,11 +9,51 @@ import * as ActionCreators from '../../actions/';
 export const RecipeDetails = (props) => {
     const { key } = props.routeParams;
     const recipe = props.recipeList.get(key);
+    const name = recipe.get('name');
+    const createdDate = recipe.get('created_date');
+    const directions = recipe.get('directions').map(direction => {
+        return (
+            <p className={style.direction}>
+                {direction}
+            </p>
+        );
+    });
+    const ingredients = recipe.get('ingredients').map(ingredient => {
+        return (
+            <p className={style.ingredient}>
+                {ingredient.get('amount')} - {ingredient.get('item')}
+            </p>
+        );
+    });
+
     return (
-        <div >
-            <h1 >
-                {recipe.get('name')}
-            </h1>
+        <div className={style.wrapper}>
+            <div className={style.intro}>
+                <img
+                    src={recipe.get('imageURL')}
+                    alt={name}
+                    className={style.image} />
+
+                <header className={style.title}>
+                    <h1 className={style.name}>
+                        {name}
+                    </h1>
+                    <h3 className={style.createdDate}>
+                        Created in: {createdDate}
+                    </h3>
+                </header>
+
+            </div>
+            <div className={style.content}>
+                <div className={style.ingrendients}>
+                    {ingredients}
+                </div>
+                <div className={style.directions}>
+                    {directions}
+                </div>
+            </div>
+
+
         </div>
     )
 }
